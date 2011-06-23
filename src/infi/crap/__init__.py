84f1452ec3e28c5e43b18b928ae142d5521662a4
Guy Rozendorn <guyr@infinidat.com>
2011-06-23 18:55:30 +0300

    refactoring and small documentation

--- src/infi/crap/__init__.py
+++ src/infi/crap/__init__.py
@@ -1,5 +1,9 @@
 __import__("pkg_resources").declare_namespace(__name__)
 
+""" working with ctypes is hard. specifically, wrapping functions is hard.
+this module helps you do that.
+"""
+
 import ctypes
 import os
 
@@ -16,6 +20,8 @@ def get_os_name():
     return os_name
 
 def errcheck_zero():
+    """ this error checker raises a RuntimeError if the returned value is zero
+    """
     def errcheck(result, func, args):
         if result == 0:
             raise RuntimeError(result)
@@ -23,6 +29,8 @@ def errcheck_zero():
     return errcheck
 
 def errcheck_nonzero():
+    """ this error checker raises a RuntimeError if the returned value is non-zero
+    """
     def errcheck(result, func, args):
         if result != 0:
             raise RuntimeError(result)
@@ -30,6 +38,8 @@ def errcheck_nonzero():
     return errcheck
 
 def errcheck_nothing():
+    """ this error checker raises no exception
+    """
     def errcheck(result, func, args):
         return args
     return errcheck
@@ -62,33 +72,55 @@ def wrap_library_function(name, library, return_value=ctypes.c_ulong, parameters
     return _function
 
 class WrappedFunction(object):
+    """ wrapping class for functions from shared libraries
+
+    In order to wrap funtion named "thisFunction" from shared library "sharedLibrary.so",
+    create a new class thisFunction that inherits from this class, and have the classmethod
+    get_library_name return the filename of the shared library to load.
+    The class method _get_library calls ctype's LoadLibrary method with what _get_library_name returns.
+
+    Do not that on UNIX systems, get_library_name requires to return a full path
+
+
+    an usage example:
+    """
+
+    # default return value is ctypes.c_ulong
+    # change if necessary when inherting
     _return_value = ctypes.c_ulong
 
     @classmethod
-    def _get_errcheck(cls):
+    def get_parameters(cls):
+        """ Each item in this tuple contains further information about a parameter, it must be a tuple containing one, two, or three items.
+
+        The first item is an integer containing a combination of direction flags for the parameter:
+
+        1
+        Specifies an input parameter to the function.
+        2
+        Output parameter. The foreign function fills in a value.
+        4
+        Input parameter which defaults to the integer zero.
+        The optional second item is the parameter name as string. If this is specified, the foreign function can be called with named parameters.
+
+        The optional third item is the default value for this parameter.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_errcheck(cls):
+        """ returns a function that is called to check the returned value of the wrapped function.
+        The error-checking function gets the following arguments from ctypes: result, func, args.
+        and should return args or raise an exception.
+        """
         return errcheck_nonzero()
 
     @classmethod
     def _get_library(cls):
-        raise NotImplementedError
-
-    @classmethod
-    def _get_parameters(cls):
-        """ Each item in this tuple contains further information about a parameter, it must be a tuple containing one, two, or three items.
-
-The first item is an integer containing a combination of direction flags for the parameter:
-
-1
-Specifies an input parameter to the function.
-2
-Output parameter. The foreign function fills in a value.
-4
-Input parameter which defaults to the integer zero.
-The optional second item is the parameter name as string. If this is specified, the foreign function can be called with named parameters.
-
-The optional third item is the default value for this parameter.
-"""
-        raise NotImplementedError
+        try:
+            return ctypes.cdll.LoadLibrary(cls.get_library_name())
+        except:
+            raise OSError
 
     @classmethod
     def __new__(cls, *args, **kwargs):
@@ -101,7 +133,7 @@ The optional third item is the default value for this parameter.
         name = cls.__name__
         parameters = cls._get_parameters()
         function = wrap_library_function(cls.__name__, cls._get_library(), cls._return_value,
-                                         parameters, cls._get_errcheck())
+                                         parameters, cls.get_errcheck())
         return function
 
     @classmethod
@@ -109,7 +141,7 @@ The optional third item is the default value for this parameter.
         try:
             function = cls._get_function()
             return True
-        except (AttributeError, OSError):
+        except (AttributeError, OSError, NotImplementedError):
             return False
         return True # pragma: no cover
 
